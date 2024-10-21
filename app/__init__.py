dd10a640f98384d667b3f8c2641b654d17bd58f9
Isaac <mugagga10@gmail.com>
2024-10-21 19:33:23 +0000

    tried to reduce circular import

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,18 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
     db.init_app(app)
     login_manager.init_app(app)
 
+    from app.models import User, Question, Option
+
     with app.app_context():
-        from . import routes
         db.create_all()
+
+    from app.routes import main
+    app.register_blueprint(main)
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 ''' Defines models for users, questions and options'''
 
-from . import db
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from app import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
