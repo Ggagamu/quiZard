57e39ef8ba40d82e94b40884a5e02a7401c8108f
Isaac <mugagga10@gmail.com>
2024-10-23 09:17:45 +0000

    edit init

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
